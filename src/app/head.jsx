23275160149a99a5215c157daeefb656f563c2ba
CrisisCoins <github@crisiscoins.com>
2023-03-02 14:17:56 -0800

    installed nextJS and created initial template

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Carbonite Search Engine V1</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Internet Browser created by Next js 13" />
+        <link rel="icon" href="/favicon.svg" />
+      </>
+    )
+  }
