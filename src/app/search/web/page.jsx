465422bd519357cafc658474d1c819f1c131b6f5
CrisisCoins <github@crisiscoins.com>
2023-03-04 18:18:25 -0800

    Completed possible search query error handeling

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,14 +1,29 @@
+import Link from "next/link";
+
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}`
   );
 
+  if (!response.ok) {
+    throw new Error("Oops! Something went wrong!");
+  }
+
   const data = await response.json();
-  console.log(data);
+
 
   const results = data.items;
 
-  return <>
-    {results && results.map(result => <h1>{result.title}</h1>)}
-    </>;
+  if(!results) {
+    return <div className="flex flex-col justify-center items-center pt-10">
+       <h1 className="text-3xl mb-4">No query results found</h1>
+       <p className="text-lg">
+        Try a different search query or return to the main homepage{" "} <Link href="/" className="text-blue-500">Home</Link>
+       </p>
+      </div>
+  }
+
+
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
